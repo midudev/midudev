76ac410ff50d5afc087105dd2755fbfe6cb847f7
midudev <miduga@gmail.com>
2020-07-18 02:10:11 +0200

    Add Youtube videos

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
+const {YOUTUBE_API_KEY} = process.env
 const fs = require('fs').promises
+const fetch = require('node-fetch')
+
 const Parser = require('rss-parser')
 const parser = new Parser()
 
-const LATEST_ARTICLE_PLACEHOLDER = /%{{latest_article}}%/g
-const ICONS_SIZE_PLACEHOLDER = /%{{icon_size}}%/g
+const NUM_OF_ARTICLES_TO_SHOW = 5
+const NUM_OF_VIDEOS_TO_SHOW = 3
 
-const ICONS_SIZE = '24px'
+const LATEST_ARTICLE_PLACEHOLDER = "%{{latest_articles}}%"
+const LATEST_YOUTUBE_VIDEOS = "%{{latest_youtube}}%"
+// const LATEST_TWEET_PLACEHOLDER = "%{{latest_tweet}}%"
+// const LATEST_INSTAGRAM_PHOTO = "%{{latest_instagram}}%"
+
+const getLatestYoutubeVideos = () => {
+  return fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UU8LeXCWOalN8SxlrPcG-PaQ&maxResults=${NUM_OF_VIDEOS_TO_SHOW}&key=${YOUTUBE_API_KEY}`)
+    .then(res => res.json())
+    .then(videos => videos.items)
+}
+
 
 ;(async () => {
-  const markdownTemplate = await fs.readFile('./README.md.tpl', { encoding: 'utf-8' })
-  const {items} = await parser.parseURL('https://midu.dev/index.xml')
-  // put the latest article
-  const [{title, link}] = items
-  const latestArticleMarkdown = `[${title}](${link})`
+  const [template, {items: articles}, videos] = await Promise.all([
+    fs.readFile('./README.md.tpl', { encoding: 'utf-8' }),
+    parser.parseURL('https://midu.dev/index.xml'),
+    getLatestYoutubeVideos()
+  ])
+
+  // create latest article markdown
+  const latestArticlesMarkdown = articles.slice(0, NUM_OF_ARTICLES_TO_SHOW)
+    .map(({title, link}) => `- [${title}](${link})`)
+    .join('\n')
+
+  // create latest youtube videos channel
+  const latestYoutubeVideos = videos
+    .map(({snippet}) => {
+      const {title, resourceId} = snippet
+      const {videoId} = resourceId
+      return `[![${title}](https://img.youtube.com/vi/${videoId}/mqdefault.jpg)](https://youtu.be/${videoId})`
+    })
+    .join('\n')
 
   // replace all placeholders with info
-  const newMarkdown = markdownTemplate
-    .replace(LATEST_ARTICLE_PLACEHOLDER, latestArticleMarkdown)
-    .replace(ICONS_SIZE_PLACEHOLDER, ICONS_SIZE) // put the icon size to all icons
-  
+  const newMarkdown = template
+    .replace(LATEST_ARTICLE_PLACEHOLDER, latestArticlesMarkdown)
+    .replace(LATEST_YOUTUBE_VIDEOS, latestYoutubeVideos)
+
   await fs.writeFile('./README.md', newMarkdown)
 })()
